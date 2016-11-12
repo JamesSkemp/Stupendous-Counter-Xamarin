7b42e060a8d3c7b8adc9d02741f27406e396eba7
James Skemp <strivinglife@gmail.com>
2016-11-12 11:29:59 -0600

    Add new view models for individual and listing of counters

---
+++ StupendousCounter.Core/ViewModel/CounterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupendousCounter.Core.ViewModel
+{
+	public class CounterViewModel : ViewModelBase
+	{
+		private readonly Counter _counter;
+		private readonly IDatabaseHelper _databaseHelper;
+
+		public CounterViewModel(Counter counter, IDatabaseHelper databaseHelper)
+		{
+			_counter = counter;
+			_databaseHelper = databaseHelper;
+		}
+
+		public string Name => _counter.Name;
+		public string Description => _counter.Description;
+		public string Value => _counter.Value.ToString("N0");
+
+		private RelayCommand _incrementCommand;
+		public RelayCommand IncrementCommand => _incrementCommand ?? (_incrementCommand = new RelayCommand(async () => await IncrementAsync()));
+
+		private async Task IncrementAsync()
+		{
+			await _databaseHelper.IncrementCounterAsync(_counter);
+			RaisePropertyChanged(() => Value);
+		}
+	}
+}
---
+++ StupendousCounter.Core/ViewModel/CountersViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupendousCounter.Core.ViewModel
+{
+	public class CountersViewModel : ViewModelBase
+	{
+		private readonly IDatabaseHelper _databaseHelper;
+
+		private readonly ObservableCollection<CounterViewModel> _counters = new ObservableCollection<CounterViewModel>();
+		public ReadOnlyObservableCollection<CounterViewModel> Counters { get; private set; }
+
+		public CountersViewModel(IDatabaseHelper databaseHelper)
+		{
+			_databaseHelper = databaseHelper;
+			Counters = new ReadOnlyObservableCollection<CounterViewModel>(_counters);
+		}
+
+		public async Task LoadCountersAsync()
+		{
+			var counters = await _databaseHelper.GetAllCountersAsync();
+			foreach (var counter in counters)
+			{
+				_counters.Add(new CounterViewModel(counter, _databaseHelper));
+			}
+		}
+	}
+}
