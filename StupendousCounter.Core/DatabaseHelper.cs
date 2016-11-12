156a34c258e6f25c36c9fa16eeb27f648a955f62
James Skemp <strivinglife@gmail.com>
2016-11-12 09:37:14 -0600

    Create database helper class

---
+++ StupendousCounter.Core/DatabaseHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupendousCounter.Core
+{
+	public class DatabaseHelper : IDatabaseHelper
+	{
+		public async Task AddOrUpdateCounterAsync(Counter counter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<Counter>> GetAllCountersAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<CounterIncrementHistory>> GetCounterHistoryAsync(int counterId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task IncrementCounterAsync(Counter counter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void CreateDatabase(string dbPath)
+		{
+
+		}
+	}
+}
---
+++ StupendousCounter.Core/IDatabaseHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupendousCounter.Core
+{
+	public interface IDatabaseHelper
+	{
+		Task AddOrUpdateCounterAsync(Counter counter);
+		Task IncrementCounterAsync(Counter counter);
+		Task<IEnumerable<Counter>> GetAllCountersAsync();
+		Task<IEnumerable<CounterIncrementHistory>> GetCounterHistoryAsync(int counterId);
+	}
+}
