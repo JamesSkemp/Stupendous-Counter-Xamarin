f6dfb80c95beb39ed993647f2068285e4cf8c60a
James Skemp <strivinglife@gmail.com>
2016-11-12 11:21:10 -0600

    Move default view models to core project

--- StupendousCounter.Core/ViewModel/MainViewModel.cs
+++ StupendousCounter.Core/ViewModel/MainViewModel.cs
@@ -1,6 +1,6 @@
 using GalaSoft.MvvmLight;
 
-namespace StupendousCounter.Droid.ViewModel
+namespace StupendousCounter.Core.ViewModel
 {
     /// <summary>
     /// This class contains properties that the main View can data bind to.
--- StupendousCounter.Core/ViewModel/ViewModelLocator.cs
+++ StupendousCounter.Core/ViewModel/ViewModelLocator.cs
@@ -16,7 +16,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
 
-namespace StupendousCounter.Droid.ViewModel
+namespace StupendousCounter.Core.ViewModel
 {
     /// <summary>
     /// This class contains static references to all the view models in the
