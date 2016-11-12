3ec5e53d802c5ca0543b0e2d1a1145cec39aa145
James Skemp <strivinglife@gmail.com>
2016-11-12 13:07:22 -0600

    Add reference to Recycler View
    Will need to upgrade to Android 5.0 libraries in order to continue.

--- StupendousCounter.Droid/Fragments/CountersFragment.cs
+++ StupendousCounter.Droid/Fragments/CountersFragment.cs
@@ -1,11 +1,14 @@
 using Android.OS;
 using Android.Support.V4.App;
+using Android.Support.V7.Widget;
 using Android.Views;
 
 namespace StupendousCounter.Droid.Fragments
 {
 	public class CountersFragment : Fragment
 	{
+		private RecyclerView _recyclerView;
+
 		public override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
