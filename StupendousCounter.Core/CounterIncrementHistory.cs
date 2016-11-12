2eb8a8c1edfcba2c1938872d152f2416b9eeb992
James Skemp <strivinglife@gmail.com>
2016-11-12 09:25:55 -0600

    Add classes for counters and history

---
+++ StupendousCounter.Core/CounterIncrementHistory.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupendousCounter.Core
+{
+	public class CounterIncrementHistory
+	{
+		[Indexed]
+		public int CounterId { get; set; }
+		public DateTime IncrementDateTimeUtc { get; set; }
+	}
+}
